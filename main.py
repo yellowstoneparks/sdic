c96d9c5514e8fee88c2553af4aa1e76041e14be4
yellowstoneparks <144966761+yellowstoneparks@users.noreply.github.com>
2024-10-31 13:19:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+# x n+1 = r*xn*(1-xn)
+# f'(xn)=r*(1−2*xn)
 
+# write code here
+x0 = 0.3348
+xn = x0
+n = 1000
+r = 3.2
+
+def der(xn):
+    return r * (1 - 2 * xn)
+
+lamb = 0
+
+for i in range(0, n):
+    xn = r * xn * (1 - xn)
+    lamb += log(abs(der(xn))) 
+
+lamb = lamb / n
+print(lamb)
