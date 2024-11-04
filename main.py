9873a2902a8982f03e8df51a0a929a942031b87b
yellowstoneparks <144966761+yellowstoneparks@users.noreply.github.com>
2024-11-04 12:47:52 -0500

    Update main.py - with graph

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+Web VPython 3.2
+
 # x n+1 = r*xn*(1-xn)
 # f'(xn)=r*(1−2*xn)
 
@@ -5,16 +7,20 @@
 x0 = 0.3348
 xn = x0
 n = 1000
-r = 3.2
+delta_r = 0.01
 
 def der(xn):
     return r * (1 - 2 * xn)
 
-lamb = 0
+r_vs_lamb = graph(title="r vs lyapunov exponent", xtitle="r", ytitle="lyapunov exponent", ymin=-10, ymax=10)
+exponents = gcurve(graph=r_vs_lamb)
 
-for i in range(0, n):
-    xn = r * xn * (1 - xn)
-    lamb += log(abs(der(xn))) 
-
-lamb = lamb / n
-print(lamb)
+for r in range(0, 7, delta_r):
+    lamb = 0
+    
+    for i in range(0, n):
+        xn = r * xn * (1 - xn)
+        lamb += log(abs(der(xn))) 
+    
+    lamb /= n
+    exponents.plot(r, lamb)
