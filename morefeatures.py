f4ccf818d1c816e21a23d4717cb8a8832b1a7393
Abigail Lin <142941671+linabigail@users.noreply.github.com>
2024-11-14 21:26:31 -0500

    Update morefeatures.py

--- morefeatures.py
+++ morefeatures.py
@@ -1,54 +1,50 @@
 Web VPython 3.2
 
-# write code here
+# basic variables
 x0 = 0.448
-xn = x0
 n = 1000
 delta_r = 0.001
 
-startofisland = None
+# island of stability variables
 startlist = []
-endofisland = None
 endlist = []
 lastlamb = None
 
-# ymin=-2, ymax=1, 
+# graph elements 
 r_vs_lamb = None
 exponents_pos = None
 exponents_neg = None
-# points of no return, when it switches from non-chaotic to chaotic and vice versa
-ponr = None
+ponr = None # points of no return, when it switches from non-chaotic to chaotic and vice versa
 
+# func to calc and plot lyapunov exp over a range of r values
 def calclyapunov(start,end,Ymin,Ymax,iscurve):
     global startlist, endlist, lastlamb, exponents_pos, exponents_neg, r_vs_lamb, ponr
-#    r_vs_lamb = graph(title="r vs lyapunov exponent", xtitle="r", ytitle="lyapunov exponent", ymin=Ymin, ymax=Ymax, xmin=start, xmax=end)
+    # initialize graph + configure based on curve style
     r_vs_lamb = graph(title="r vs lyapunov exponent", xtitle="r", ytitle="lyapunov exponent", fast=False, width=800, ymin=Ymin, ymax=Ymax, xmin=start,xmax=end)
     if iscurve == 0:
-        exponents_pos = gdots(graph = r_vs_lamb, size=2, color=color.green)
-        exponents_neg = gdots(graph = r_vs_lamb, size=2, color=color.blue)
+        exponents_pos = gdots(graph = r_vs_lamb, size=2, color=color.orange, label = "Positive Lyapunov Exponent")
+        exponents_neg = gdots(graph = r_vs_lamb, size=2, color=color.blue, label = "Negative Lyapunov Exponent")
         ponr = gdots(graph = r_vs_lamb, size = 3, color = color.red, label = "Point of No Return")
     else:
-        exponents_pos = gdots(graph = r_vs_lamb, size=5, color=color.green)
-        exponents_neg = gdots(graph = r_vs_lamb, size=5, color=color.blue)
-        ponr = gdots(graph = r_vs_lamb, size = 7, color = color.red, label = "Point of No Return")
+        exponents_pos = gdots(graph = r_vs_lamb, size=12, color=color.orange, label = "Positive Lyapunov Exponent")
+        exponents_neg = gdots(graph = r_vs_lamb, size=12, color=color.blue, label = "Negative Lyapunov Exponent")
+        ponr = gdots(graph = r_vs_lamb, size = 15, color = color.red, label = "Point of No Return")
+    # loop to calc lyapunov
     for r in range(start, end, delta_r):
         def der(xn):
             return r * (1 - 2 * xn)
-    
         lamb = 0
         xn = x0
-        
         for i in range(0, n):
             xn = r * xn * (1 - xn)
             lamb += log(abs(der(xn))) 
-        
         lamb /= n
-        
+        # plot lyapunov based on sign
         if lamb > 0:
             exponents_pos.plot(r, lamb)
         else:
             exponents_neg.plot(r, lamb)
-#         island of stability appears when lyapunov expnent becomes negative
+        # find islands of stability
         if lastlamb is not None:
             if lamb < 0 and lastlamb > 0:
                 endofisland = round(r - delta_r,3)
@@ -59,16 +55,20 @@ def calclyapunov(start,end,Ymin,Ymax,iscurve):
                 endlist.append(startofisland)
                 ponr.plot(startofisland,lamb)
         lastlamb = lamb
-    
+
+# initial lyapunov calc
 calclyapunov(0,4,-1,1,0)
+
 endlist = endlist[1:]
 print("Starts of Island of Stability: ",startlist)
 print("Ends of Island of Stability: ",endlist)
 
-resetButton = button(bind = reset, text = "Click to examine island of stability", pos=scene.title_anchor)
+# button to zoom in on islands of stability
+islandButton = button(bind = island, text = "Click to examine island of stability", pos=scene.title_anchor)
 index = 0
+scene.delete()
 
-def reset(evt):
+def island(evt):
     global ponr, exponents_pos, exponents_neg, r_vs_lamb, index
     if startlist and endlist and index < len(startlist):
         print("Islands of Stability - Start:", startlist[index], "End:", endlist[index])
